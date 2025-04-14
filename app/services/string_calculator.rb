b7867d8a87d8a681156aa8abed0f73131ff1f1e7
drashtirathod4 <drashti.r@simformsolutions.com>
2025-04-14 22:20:59 +0530

    feat: Implement add method to handle custom delimiters

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,6 +2,16 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/[\n,]/).map(&:to_i).sum
+    # Check for custom delimiter
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers.split("\n", 2)
+      delimiter = delimiter[2..-1] # Extract the delimiter
+      numbers = numbers.split(delimiter).map(&:to_i).sum
+    else
+      # Handle default comma and newline delimiters
+      numbers = numbers.split(/[\n,]/).map(&:to_i).sum
+    end
+
+    numbers
   end
 end
