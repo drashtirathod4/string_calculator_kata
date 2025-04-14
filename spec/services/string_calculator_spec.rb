2bd02ebce47502ff42b949be69f282e7d39689fd
drashtirathod4 <drashti.r@simformsolutions.com>
2025-04-14 22:14:54 +0530

    feat: Add test case for custom delimiter

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe StringCalculator do
     it "returns the sum of numbers with newlines between them" do
       expect(described_class.add("1\n2,3")).to eq(6)
     end
+
+    it "returns the sum of numbers with a custom delimiter" do
+      expect(described_class.add("//;\n1;2")).to eq(3)
+    end
   end
 end
